7dfaa07a56d1223a95960fc5db2cb1a9555eaede
Владислав Стяжкин <vladder2312@mail.ru>
2021-07-22 16:40:46 +0300

    Перенос ссылки в URLConstants

--- app/src/main/java/ru/vladder2312/filmcatalog/data/URLConstants.kt
+++ app/src/main/java/ru/vladder2312/filmcatalog/data/URLConstants.kt
@@ -2,6 +2,7 @@ package ru.vladder2312.filmcatalog.data
 
 object URLConstants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val COVERS_ULS = "https://image.tmdb.org/t/p/w780"
     const val GET_MOVIES = "discover/movie"
     const val SEARCH_MOVIES = "search/movie"
 }
